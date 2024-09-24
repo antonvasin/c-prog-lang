03696ff4dfcc918755c0d9118cdd3bfd8b281d4e
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2024-09-24 23:26:04 +0100

    ex 1-3

---
+++ chapter1/1_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  float fah, cel;
+  float lower, upper, step;
+  lower = 0;
+  upper = 300;
+  step = 20;
+  fah = lower;
+  printf(" F     C\n");
+  printf("-----------\n");
+  while (fah <= upper) {
+    cel = (5.0/9.0) * (fah-32.0);
+    printf("%3.0f %6.1f\n", fah, cel);
+    fah = fah + step;
+  }
+}
