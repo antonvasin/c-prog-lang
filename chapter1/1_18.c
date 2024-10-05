1ed11191d64f90dbc9e19d5e73a18b6931f059bc
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2024-10-05 18:26:21 +0100

    ex 1.18

---
+++ chapter1/1_18.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int getln(char[], int);
+
+int main()
+{
+  int len;
+  char line[MAXLINE];
+  while ((len = getln(line, MAXLINE)) > 0)
+    if (len > 0) printf("%d - %s\n", len, line);
+
+  return 0;
+}
+
+int getln(char s[], int max)
+{
+  int i, c;
+  int tc = -1;
+  for (i=0; (c=getchar()) != EOF &c!='\n'; ++i) {
+    if (c == '\t' || c == ' ') {
+      /* mark potential position for new end of line */
+      if (tc == -1) {
+        tc = i;
+      }
+    } else if (tc > -1) {
+      tc = -1;
+    }
+    if (i < max-1) s[i] = c;
+  }
+  if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+  /* mark new end of line if encountered trailing chars */
+  if (tc > -1) i = tc;
+  s[i] = '\0';
+  return i;
+}
